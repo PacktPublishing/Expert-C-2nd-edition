4b47970b39f2099a70adcab7dee213e8d6b357c2
Araks8 <arakstigranyan8@gmail.com>
2022-10-19 15:20:46 +0400

    Chapter 6

---
+++ 17_sort.cpp
@@ -0,0 +1,24 @@
+
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <iostream>
+
+int main()
+{
+  std::vector<int> vec;
+  // insert elements into the vector
+  vec.push_back(4);
+  vec.push_back(11);
+  vec.push_back(9);
+  for (auto& v : vec) {
+    std::cout << v << " ";
+  }
+  std::cout << std::endl;
+
+  std::sort(vec.begin(), vec.end());
+  for (auto& v : vec) {
+    std::cout << v << " ";
+  }
+  std::cout << std::endl;
+}
