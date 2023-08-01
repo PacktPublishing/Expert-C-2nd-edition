b72194a28950eae84226b2ceb9a80f4813a0099a
Araks8 <78375584+Araks8@users.noreply.github.com>
2023-08-01 22:50:06 +0100

    Update 3_count_evens_functional.cpp

--- Chapter08/3_count_evens_functional.cpp
+++ Chapter08/3_count_evens_functional.cpp
@@ -1,22 +1,23 @@
+#include <iostream>
+#include <algorithm>
 #include <vector>
 #include <ranges>
-#include <iostream>
 
 using IntMatrix = std::vector<std::vector<int>>;
 
 int count_evens(const std::vector<int>& number_line) {
-  return std::count_if(number_line.begin(), 
-       number_line.end(), [](int num){return num % 2 == 0;});
+    return std::count_if(number_line.begin(),
+        number_line.end(), [](int num) {return num % 2 == 0; });
 }
 
 std::vector<int> count_all_evens(const IntMatrix& numbers)
 {
-  return numbers | std::ranges::views::transform(count_evens);
+    return numbers | std::views::transform(count_evens);
 }
 
 int main()
 {
-    IntMatrix m{{1, 2, 3}, {4, 5, 6}};
+    IntMatrix m{ {1, 2, 3}, {4, 5, 6} };
     for (auto item : count_all_evens(m)) {
         std::cout << item << " ";
     }
