283a4e2eed60a5bd8489bec3c405a148ed8582fc
Araks8 <78375584+Araks8@users.noreply.github.com>
2023-08-01 22:04:51 +0100

    Update 15_scope.cpp

--- Chapter02/15_scope.cpp
+++ Chapter02/15_scope.cpp
@@ -1,7 +1,15 @@
+#include <iostream>
+#include <string>
+
 class Product {
 public:
   Product() = default;
   // ...
+
+  // Add members to the Product class
+  std::string name;
+  int rating;
+  double price;
 };
 
 static Product global_prod;
@@ -15,9 +23,18 @@ Product* foo() {
 int main() {
   Product stack_prod;
   if (true) {
-    Product tmp;
-    tmp.rating = 3;
-  }
-  stack_prod.price = 4.2;
-  foo();
+        Product tmp;
+        tmp.rating = 3;
+    }
+    stack_prod.price = 4.2;
+    Product* returned_product = foo();
+
+    // Printing the details of the returned product
+    std::cout << "Name: " << returned_product->name << std::endl;
+    std::cout << "Rating: " << returned_product->rating << std::endl;
+    std::cout << "Price: " << returned_product->price << std::endl;
+
+    delete returned_product;
+
+    return 0;
 }
