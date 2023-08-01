2f02d0ee5f3754ebfacd41615689e5e76f6d8c8c
Araks8 <78375584+Araks8@users.noreply.github.com>
2023-08-01 22:30:59 +0100

    Update 16_search_vector.cpp

--- Chapter 06/16_search_vector.cpp
+++ Chapter 06/16_search_vector.cpp
@@ -1,10 +1,28 @@
+#include <iostream>
+#include <vector>
+
 template <typename T>
 int search(const std::vector<T>& vec, const T& item)
 {
-  for (int ix = 0; ix < vec.size(); ++ix) {
-    if (vec[ix] == item) {
-      return ix;
+    for (int ix = 0; ix < vec.size(); ++ix) {
+        if (vec[ix] == item) {
+            return ix;
+        }
     }
-  }
-  return -1; // not found
+    return -1; // not found
+}
+
+int main()
+{
+    std::vector<int> intVector = { 1, 2, 3, 4, 5 };
+    int itemToFind = 3;
+    int index = search(intVector, itemToFind);
+    if (index != -1) {
+        std::cout << "Item found at index: " << index << std::endl;
+    }
+    else {
+        std::cout << "Item not found." << std::endl;
+    }
+
+    return 0;
 }
