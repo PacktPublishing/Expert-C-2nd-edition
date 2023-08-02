190576c948f508c61691f1828916d3753886dec3
Araks8 <78375584+Araks8@users.noreply.github.com>
2023-08-02 22:42:39 +0100

    Update 7_transform_sqrt.cpp

--- Chapter08/7_transform_sqrt.cpp
+++ Chapter08/7_transform_sqrt.cpp
@@ -6,7 +6,7 @@
 int main()
 {
     std::vector<double> vec{ 1.1, 2.2, 4.3, 5.6, 2.4 };
-    auto result = vec | std::views::transform(std::sqrt);
+    auto result = vec | std::views::transform(static_cast<double (*)(double)>(std::sqrt));
 
     for (const auto& value : result)
     {
